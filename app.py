40a3bfd4ca5686437332913e7264735b99e6ca42
hardwarepro <92922224+scooterthedev@users.noreply.github.com>
2025-08-12 14:53:15 -0400

    working on a fix

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 API_KEY = os.getenv("API_KEY")
 URL = "https://api.tomorrow.io/v4/weather/realtime"
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/get-location', methods=['POST'])
 def location():
-    if request.method == 'POST':
-        city = request.form.get('city')
+    city = request.form.get('city')
+    if not city:
+        return render_template('index.html', error=True)
+        
         try:
             geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}"
             geo_resp = requests.get(geo_url).json()
@@ -22,15 +24,12 @@ def location():
             if "results" in geo_resp and len(geo_resp["results"]) > 0:
                 lat = geo_resp["results"][0]["latitude"]
                 lon = geo_resp["results"][0]["longitude"]
-
+                return redirect(url_for('weather', city=city, lat=lat, lon=lon))
             else:
                 print(f"No city found")
 
         except Exception as e:
             print(f"Error occurred: {e}")
-
-    return lat, lon
-
 
 @app.route('/get-weather', methods=['GET'])
 def weather(lat, long):
